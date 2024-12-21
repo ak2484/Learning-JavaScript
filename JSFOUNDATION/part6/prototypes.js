b70c8f29134b94dad78d60063f098bce213ca8bf
Akash <akashnahak736@gmail.com>
2024-12-21 13:28:38 +0530

    prototypes commited

--- JSFOUNDATION/part6/prototypes.js
+++ JSFOUNDATION/part6/prototypes.js
@@ -9,6 +9,22 @@ let lenovo = {
 
 let tomHardware = {};
 
-console.log(`lenovo`, lenovo.__proto__);
+// console.log(`lenovo`, lenovo.__proto__);
 
 // '__' is called as Dunder
+
+///// if you don't want to use the __proto__ then you can to it as follows
+let genericCar = {
+  tyres: 4,
+};
+
+let tesla = {
+  driver: "AI",
+};
+
+Object.setPrototypeOf(tesla, genericCar);
+
+// console.log(`tesla`, Object.getPrototypeOf(tesla));
+// console.log(tesla.hasOwnProperty("driver")); // it rturns a boolean value
+
+tesla.mode = "electric";
