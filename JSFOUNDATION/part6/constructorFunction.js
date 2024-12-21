f8806110820deeadd4a39791108d5b3ca1407adc
Akash <akashnahak736@gmail.com>
2024-12-21 13:58:37 +0530

    Cnstructor Function Commit

---
+++ JSFOUNDATION/part6/constructorFunction.js
@@ -0,0 +1,50 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+let myCar = new Car("Toyota", "Camry");
+// console.log(myCar);
+
+let myNewCar = new Car("Tata", "Safari");
+// console.log(myNewCar);
+
+function Tea(type) {
+  this.type = type;
+  this.describe = function () {
+    return `this ia a cup of ${this.type}`;
+  };
+}
+
+let mytea = new Tea("Lemon tea");
+// console.log(mytea.describe());
+
+// constructor function with prototypes
+function Animal(species) {
+  this.species = species;
+}
+
+Animal.prototype.sound = function () {
+  return `${this.species} makes a sound`;
+};
+
+let dog = new Animal("Dog");
+// console.log(dog.sound());
+
+let cat = new Animal("Cat");
+// console.log(cat.sound());
+
+function Drink(name) {
+  if (!new.target) {
+    throw new Error("Drink must be called with new keyword");
+  }
+  this.name = name;
+}
+
+let tea = new Drink("tea");
+let coffee = Drink("coffee");
